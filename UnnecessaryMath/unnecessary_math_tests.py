59904f3bf3a7c215ff60b9dabb0e566a8f30aa54
jdmac020 <john.macgregor@gmail.com>
2018-12-30 09:09:38 -0500

    One Passing One Failing for Contrast

--- UnnecessaryMath/unnecessary_math_tests.py
+++ UnnecessaryMath/unnecessary_math_tests.py
@@ -1,5 +1,18 @@
 import unittest
-import unnecessary_math
+import unnecessary_math as testClass
 
-print("It Works!")
-input("Press Enter To Continue...")
+class TestUnnecessaryMath(unittest.TestCase):
+
+	def setUp(self):
+		pass
+		
+	def test_multiplyThreeAndFour_expectTweleve(self):
+		self.assertEqual( testClass.multiply(3,4), 12)
+		
+	def test_multiplyThreeAndFive_expectFifteen(self):
+		self.assertEqual( testClass.multiply(3,5), 12)
+		
+# actually runs the tests and provides the output
+# run from command line with the -v flag for verbose output--why a test failed, what line, etc
+if __name__ == '__main__':
+	unittest.main()
